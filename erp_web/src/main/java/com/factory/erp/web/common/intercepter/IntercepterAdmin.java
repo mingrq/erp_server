72ba94748cedd45ea32ce4557fe4b597557abafb
yuwlong <long10102>
2020-08-07 21:49:12 +0800

    配置文件修改

---
+++ erp_web/src/main/java/com/factory/erp/web/common/intercepter/IntercepterAdmin.java
@@ -0,0 +1,9 @@
+package com.factory.erp.web.common.intercepter;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * 后台用户登录与权限拦截器
+ */
+public class IntercepterAdmin extends HandlerInterceptorAdapter {
+}
